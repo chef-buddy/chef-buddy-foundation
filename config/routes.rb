db8bf6f32a4eed3f1840685758f5e3c38c9065e0
Solomon Hilliard <shillia6@eagles.nccu.edu>
2015-03-22 18:03:29 -0400

    Route changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   get 'home/show'
-  root 'home#index'
+  root 'users#index'
   resources :recipes
-
+  devise_for :users, controllers: {registrations: 'registrations'}
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
