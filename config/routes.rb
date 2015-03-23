9a73aca9bb66a0e25ea52a5875d703a950eda9bc
Ashley Hightman <ahightman@gmail.com>
2015-03-22 22:41:48 -0400

    styling user page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'home/show'
+  get 'home/index'
   root 'users#index'
   resources :recipes
   devise_for :users, controllers: {registrations: 'registrations'}
