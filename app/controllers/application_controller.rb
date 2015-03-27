43fa3027d1eccd9aa1754a3b9b6259796d6be251
Solomon Hilliard <shillia6@eagles.nccu.edu>
2015-03-27 11:29:34 -0400

    stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,9 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
+
   def after_sign_in_path_for(resource)
-    sign_in_url = new_user_session_url
-    if request.referer == sign_in_url
-      super
-    else
-      stored_location_for(resource) || request.referer || root_path
-    end
+    session["user_return_to"] || users_path
   end
 
   protected
