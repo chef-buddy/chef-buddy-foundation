049b2d3dd00e405687cbece44788cd9dfee2df71
Ashley Hightman <ahightman@gmail.com>
2015-03-27 21:42:13 -0400

    attempt to create modal functionality

--- app/assets/javascripts/recipeCard.js
+++ app/assets/javascripts/recipeCard.js
@@ -8,3 +8,16 @@
 //       $('.fond').hide(); // hide it when it is done.
 //   });
 // });
+
+//Follow Button Effect
+
+$(document).ready(
+
+	function iniciar(){
+	$('.follow').on("click", function(){
+		$('.follow').css('background-color','#34CF7A');
+		$('.follow').html('<div class="icon-ok"></div> Following');
+	});
+	}
+
+);
