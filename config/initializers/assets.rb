e7b946553a4b3e80e57bc14785a5e75fb20494ba
Solomon Hilliard <shillia6@eagles.nccu.edu>
2015-04-02 11:32:37 -0400

    Add Assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,5 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += ['recipeCard.js', 'initialMessage.js','recipe_list.js',
+'home.coffee', 'recipes.coffee', 'users.coffee']
